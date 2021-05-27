0f29c23bd748f98b44506fd0eed044b58e7d79b0
self067 <seltor@mail.ru>
2021-05-27 23:38:10 +0300

    doc: add jsdoc 2 board.service

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,9 +1,35 @@
+/**
+ * Boards data
+ */
 const boardsRepo = require('./board.memory.repository');
-
+/**
+ * Get all boards
+ * @returns {Board[]} Returns all boards
+ */
 const getAll = () => boardsRepo.getAll();
-const getBoardById = id => boardsRepo.getBoardById(id);
-const postBoard = board => boardsRepo.postBoard(board);
-const putBoard = board => boardsRepo.putBoard(board);
-const deleteBoard = id => boardsRepo.deleteBoard(id);
+/**
+ * Gets the Board by ID
+ * @param {uuid} BoardId
+ * @returns {Board}
+ */
+const getBoardById = (id) => boardsRepo.getBoardById(id);
+/**
+ * Creates a new board
+ * @param {Board} new board
+ * @returns {Board}
+ */
+const postBoard = (board) => boardsRepo.postBoard(board);
+/**
+ * Update board
+ * @param {Board} updated board
+ * @returns {Board}
+ */
+const putBoard = (board) => boardsRepo.putBoard(board);
+/**
+ * Delete board
+ * @param {BoardId} Board Id
+ * @return {boolean}
+ */
+const deleteBoard = (id) => boardsRepo.deleteBoard(id);
 
 module.exports = { getAll, getBoardById, postBoard, putBoard, deleteBoard };
