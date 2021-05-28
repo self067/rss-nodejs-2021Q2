7cc3d0920da2daf946474f37023e1f15b15378d1
self067 <seltor@mail.ru>
2021-05-29 01:54:51 +0300

    feat: add jsdoc 2 columns

--- src/resources/column/column.model.js
+++ src/resources/column/column.model.js
@@ -6,10 +6,12 @@ const { v4: uuidv4 } = require('uuid');
 class Column {
   /**
    * @constructor
-   * @param {(id,title,columns)}
+   * @param  {string} id Column Id
+   * @param  {string} title Column title
+   * @param  {number} order Order
    */
 
-  constructor({ id = uuidv4(), title = 'COLUMN', order = 'order' } = {}) {
+  constructor({ id = uuidv4(), title = 'COLUMN', order = 0 } = {}) {
     this.id = id;
     this.title = title;
     this.order = order;
--- src/resources/column/column.service.js
+++ src/resources/column/column.service.js
@@ -1,15 +1,56 @@
+/**
+ * Columns data
+ * @namespace Columns/Service
+ */
+
+/**
+ * Columns data repository
+ * @memberof Columns/Service
+ */
 const columnsRepo = require('./column.memory.repository');
 
+/**
+ * Get all columns
+ * @memberof Columns/Service
+ * @returns {Column[]} Returns all columns
+ *
+ */
 const getAll = () => columnsRepo.getAll();
-const getColumnById = id => columnsRepo.getColumnById(id);
-const postColumn = column => columnsRepo.postColumn(column);
-const putColumn = column => columnsRepo.putColumn(column);
-const deleteColumn = id => columnsRepo.deleteColumn(id);
+/**
+ * Get column by ID
+ * @memberof Columns/Service
+ * @param {Id} id Column ID
+ * @returns {Column}
+ */
+const getColumnById = (id) => columnsRepo.getColumnById(id);
+/**
+ * Create column
+ * @memberof Columns/Service
+ * @param {object} column New Column
+ * @returns {Column}
+ */
+const postColumn = (column) => columnsRepo.postColumn(column);
+
+/**
+ * Update column
+ * @memberof Columns/Service
+ * @param {object} column Column object
+ * @returns {Column}
+ */
+const putColumn = (column) => columnsRepo.putColumn(column);
+
+/**
+ * Delete column
+ * @memberof Columns/Service
+ * @param {Id} id Column ID
+ * @returns {boolean}
+ */
+const deleteColumn = (id) => columnsRepo.deleteColumn(id);
 
 module.exports = {
   getAll,
   getColumnById,
   postColumn,
   putColumn,
-  deleteColumn
+  deleteColumn,
 };
