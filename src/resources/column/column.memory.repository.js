d84e02bff14cfe60d52be7fe3b9901d8aabc632e
self067 <seltor@mail.ru>
2021-05-28 16:55:52 +0300

    feat: add jsdoc 2 column modules

--- src/resources/column/column.memory.repository.js
+++ src/resources/column/column.memory.repository.js
@@ -1,22 +1,43 @@
-const columns = [];
+/**
+ * Boards data
+ * @namespace Columns/Repository
 
+ */
+
+/**
+ * Boards array
+ * @memberof Boards/Repository
+ */
+const columns = [];
+/**
+ * Get all columns
+ * @memberof Columns/Repository
+ * @returns {Column[]} Returns all columns
+ */
 const getAll = async () => columns;
-const getColumnById = async id => columns.find(item => item.id === id);
+/**
+ * Get the Column by ID
+ * @memberof Column/Repository
+ * @param {uuid} ColumnId
+ * @returns {Column} Column object
+ *
+ */
+const getColumnById = async (id) => columns.find((item) => item.id === id);
 
 const postColumn = async ({ id, name, login, password }) => {
   columns.push({ id, name, login, password });
   return { id, name, login, password };
 };
 
-const deleteColumn = async id => {
-  const index = columns.findIndex(item => item.id === id);
+const deleteColumn = async (id) => {
+  const index = columns.findIndex((item) => item.id === id);
   if (index === -1) return false;
   columns.splice(index, 1);
   return true;
 };
 
 const putColumn = async ({ id, title, order }) => {
-  const index = columns.findIndex(item => item.id === id);
+  const index = columns.findIndex((item) => item.id === id);
   if (index === -1) return null;
   columns[index].title = title;
   columns[index].order = order;
--- src/resources/column/column.model.js
+++ src/resources/column/column.model.js
@@ -1,6 +1,14 @@
 const { v4: uuidv4 } = require('uuid');
-
+/**
+ * Column data model
+ * @class
+ */
 class Column {
+  /**
+   * @constructor
+   * @param {(id,title,columns)}
+   */
+
   constructor({ id = uuidv4(), title = 'COLUMN', order = 'order' } = {}) {
     this.id = id;
     this.title = title;
